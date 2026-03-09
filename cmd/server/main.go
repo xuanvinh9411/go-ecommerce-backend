6adf3a3ae00e2b6cda4515201143af03d91fe0ae
Xuân Vĩnh Lê <xuanvinhle@Xuans-MacBook-Air.local>
2026-03-09 15:22:13 +0700

    update setup routers

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,13 @@
-package server
+package main
 
-import "fmt"
+import "github.com/xuanvinh9411/go-ecommerce-backend/internal/routers"
+
 
 func main() {
-	fmt.Println("Starting server...")
+	// Create a new Gin router
+	r := routers.NewRouter()
+	
+	r.Run(":3000") // Start the server on port 3000
+	
 }
+
